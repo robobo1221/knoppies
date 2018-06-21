f57e41f41e91eebb99a3ecb63b1aa74abaf231d4
robobo1221 <rstikfort@gmail.com>
2018-06-21 14:14:58 +0200

    Finished

--- scripts.js
+++ scripts.js
@@ -1,22 +1,73 @@
-function isDigit(str) {
-    return str && !/[^\d]/.test(str);
-}
-
 document.getElementById("addCharAmount").onclick = function() {;
 
     var inputText = document.getElementById("textAdd").value;
+        inputText = inputText.replace(/[\d\s]/g,'');
+
     var inputTextLength = inputText.length;
 
-    var charCount = 0;
+    var node = document.createElement("li");
+    var text = document.createTextNode(inputTextLength);
+    node.appendChild(text);
 
-    for( var i = 0; i < inputTextLength; i++ )
-    {
-        if( isDigit(inputText) )
-            charCount++;
-    }
+    var list = document.getElementById("list");
+    var isChild = document.getElementById("list").hasChildNodes();
+
+    list.appendChild(node);
+
+    if(isChild) list.removeChild(list.childNodes[0]);
+}
+
+document.getElementById("addReverseText").onclick = function() {;
+
+    var inputText = document.getElementById("textAdd").value;
+        inputText = inputText.replace(/[0-9]/g,'');
+
+    //Reverse text
+    var splitText = inputText.split("");
+    var reverseArray = splitText.reverse();
+    var joinedArray = reverseArray.join("");
 
     var node = document.createElement("li");
-    var text = document.createTextNode(inputTextLength - charCount);
+    var text = document.createTextNode(joinedArray);
+    node.appendChild(text);
+
+    var list = document.getElementById("list");
+    var isChild = document.getElementById("list").hasChildNodes();
+
+    list.appendChild(node);
+
+    if(isChild) list.removeChild(list.childNodes[0]);
+}
+
+function isUpperCase(char){
+    return char != char.toUpperCase();
+}
+
+function convertCase(string){
+    var stringLength = string.length;
+    var textArray = string.split("");
+
+    for (var i = 0; i < stringLength; i++){
+        var textPosition = textArray[i];
+
+        if (isUpperCase(textPosition)){
+            textArray[i] = textPosition.toUpperCase();
+        } else {
+            textArray[i] = textPosition.toLowerCase();
+        }
+    }
+
+    return textArray.join("");
+}
+
+document.getElementById("addSwitchCases").onclick = function() {;
+
+    var inputText = document.getElementById("textAdd").value;
+        inputText = convertCase(inputText);
+
+    var node = document.createElement("li");
+    var text = document.createTextNode(inputText);
+
     node.appendChild(text);
 
     var list = document.getElementById("list");
