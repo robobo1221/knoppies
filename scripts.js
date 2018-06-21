5abcf5be77cae632cf198f28ba4bbc556feb5e7d
robobo1221 <rstikfort@gmail.com>
2018-06-21 11:54:39 +0200

    More stuff

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
-function getValue()
-{
-    document.getElementById('textAdd').value
-}
+document.getElementById("addCharAmount").onclick = function() {
+    //First things first, we need our text:
+    var text = document.getElementById("textAdd").value; //.value gets input values
 
-var text = getValue();
-var li = "<li>""
+    //Now construct a quick list element
+    var li = "<li>" + text + "</li>";
+
+    //Now use appendChild and add it to the list!
+    document.getElementById("list").appendChild(li);
+}
